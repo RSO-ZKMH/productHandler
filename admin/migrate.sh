6922262a6528fa44a515cb050380e52bd3cd5b85
zan.korosak <zan.korosak@xlab.si>
2022-11-16 20:37:52 +0100

    Update.

--- admin/migrate.sh
+++ admin/migrate.sh
@@ -5,5 +5,5 @@ SUPERUSER_USERNAME=${DJANGO_SUPERUSER_USERNAME:-"admin"}
 SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD:-"Password1"}
 cd /app/
 
-python manage.py migrate
-python manage.py createsuperuser --email $SUPERUSER_EMAIL --password $SUPERUSER_PASSWORD --username $SUPERUSER_USERNAME --noinput || true
+/opt/venv/bin/python manage.py migrate --noinput
+/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --password $SUPERUSER_PASSWORD --username $SUPERUSER_USERNAME --noinput || true
