6de8df320033d6e295639062ad4f653ae109cf00
zan.korosak <zan.korosak@xlab.si>
2022-12-04 13:15:39 +0100

    Handled static files for development.

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -89,7 +89,6 @@ DATABASES = {
     }
 }
 
-print(DATABASES)
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -125,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
