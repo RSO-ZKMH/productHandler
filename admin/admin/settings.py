f4e512754776a0558c240b14be6e4a3ecf585b8f
zan.korosak <zan.korosak@xlab.si>
2023-01-04 18:02:26 +0100

    Added products to static root.

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = 'products/static/'
 
 
 print(BASE_DIR, STATIC_URL)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'products/static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
