4524fee61afe928761d1b7f9fa5f6dbd7e973df9
zan.korosak <zan.korosak@xlab.si>
2023-01-05 12:30:47 +0100

    Added another static url.

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
 
 SWAGGER_SETTINGS = {
    'USE_SESSION_AUTH': False,
-    'url': '/products/static/drf-yasg/'
+    'url': '/products/static/swagger-ui/'
 }
 
 # Password validation
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/products/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
